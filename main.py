a21c86fd7ec93a3da2fbcd183511eecb98ebe0cf
JMallone <michels@alunos.utfpr.edu.br>
2021-08-17 23:35:54 -0300

    Melhorando o recover

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import os
-from discManager2 import cDISC_MANAGER
+from discManager import cDISC_MANAGER
 
 def main():
     cDISC = cDISC_MANAGER()
     #cDISC.add_in_folder_on_disc("Juliano", {})
     #cDISC.add_in_file_on_disc("codigo.pdf", {"codigo.pdf": "Ola"}, 20, 3)
-    #cDISC.add_in_file_on_disc("codigo.pdf", 20, 1)
+    #cDISC.add_in_file_on_disc("michel.txt")
     cDISC.recover_file_on_disc([0])
-    cDISC.view_disc_data()
+    #cDISC.view_disc_data()
     #cDISC.save_disc()
     exit()
 
