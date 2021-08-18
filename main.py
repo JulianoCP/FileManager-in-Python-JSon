c1580d746a49dcab43c6da7f5f57292c9630e2d8
JMallone <michels@alunos.utfpr.edu.br>
2021-08-17 22:19:02 -0300

    Adicionando recover file

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from discManager import cDISC_MANAGER
+import os
+from discManager2 import cDISC_MANAGER
 
 def main():
     cDISC = cDISC_MANAGER()
     #cDISC.add_in_folder_on_disc("Juliano", {})
-    cDISC.add_in_file_on_disc("juliano.txt", {"juliano.txt": "Ola"}, 20, 3)
+    #cDISC.add_in_file_on_disc("codigo.pdf", {"codigo.pdf": "Ola"}, 20, 3)
+    #cDISC.add_in_file_on_disc("codigo.pdf", 20, 1)
+    cDISC.recover_file_on_disc([0])
     cDISC.view_disc_data()
-    cDISC.save_disc()
+    #cDISC.save_disc()
     exit()
 
 if __name__ == '__main__': 
