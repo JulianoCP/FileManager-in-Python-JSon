130bc951034daa899d57d0e6c10276313d2d8d13
Juliano Cesar Petini <julianopetini@alunos.utfpr.edu.br>
2021-08-24 09:41:09 -0300

    fix

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from commandManager import cCOMMAND_MANAGER
 
 def main():
     cCOMMAND = cCOMMAND_MANAGER()
+    curent_folder = "/"
 
     while(True):
-        cmd = input(" # ")
+        cmd = input("SO-UTFPR@2021: <" + curent_folder + "> # ")
         cmd = cmd.split(" ")
         cCOMMAND.command_line(cmd)
+        curent_folder = cCOMMAND.cDISK.return_correct_context(cCOMMAND.cDISK.current_folder)
 
 if __name__ == '__main__':
     main()
